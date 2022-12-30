783957e3d557fd63b0fd2ce55125da64d86aca9d
John Diamond <jdiamond@stalag13.net>
2022-12-30 10:50:23 -0600

    GREEN: Made the first unit test pass.

--- telephone-index/src/App.test.js
+++ telephone-index/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Given nothing, when launched, then the header is rendered', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Telephone Index/i);
   expect(linkElement).toBeInTheDocument();
 });
