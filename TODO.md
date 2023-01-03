87dff009f6a14ccb55dc71143d8f1189657e3950
John Diamond <jdiamond@stalag13.net>
2023-01-03 12:54:30 -0600

    Re-generated telephone-index with the typescript template.

--- TODO.md
+++ TODO.md
@@ -1,7 +1,7 @@
 # TODO List
 
 * ~~Acceptance test: first test~~
-* Create a GitHub Actions workflow
-* Refactor: reconfigure React to use TypeScript??
+* ~~Create a GitHub Actions workflow~~
+* ~~Refactor: reconfigure React to use TypeScript~~
 * Acceptance test: initial page has index and search 
 * Acceptance test: initial page has no results shown
---
+++ telephone-index/src/App.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  useEffect( () => {
+    document.title = 'Telephone Index'
+  }, [] );
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Telephone Index </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
