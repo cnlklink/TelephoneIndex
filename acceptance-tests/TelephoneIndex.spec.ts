87854d0eca2535dad24f80bd18604e55c6b6d059
John Diamond <jdiamond@stalag13.net>
2023-01-03 14:16:37 -0600

    GREEN - made the test pass with a niave implementation of the results display

--- acceptance-tests/TelephoneIndex.spec.ts
+++ acceptance-tests/TelephoneIndex.spec.ts
@@ -12,19 +12,19 @@ test( 'Navigate to the home page, telephone index is displayed with no results',
 
 async function navigatePageToHome( page: Page )
 {
+  // I navigate to the application home page
   await page.goto( 'http://localhost:3000/' )
 }
 
 async function assertTelephoneIndexIsDisplayedOn( on: Page )
 {
-  // Then...
   // I should see the title banner
   await expect( on ).toHaveTitle( /telephone index/i )
 }
 
 async function assertThereAreNoResultsDisplayedOn( on: Page )
 {
-  // Then...
   // I should see a "No results found" message
-  await expect( on ).toContain( /no results found/i )
+  const locator = on.locator( '#search-results-where' )
+  await expect( locator ).toHaveText( /no results found/i )
 }
--- telephone-index/src/App.tsx
+++ telephone-index/src/App.tsx
@@ -7,12 +7,14 @@ function App() {
   }, [] );
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="Telephone-Index-App">
+      <header className="Telephone-Index-App-header">
         <h1> Telephone Index </h1>
       </header>
 
-      <p> No results found. </p>
+      <div id="search-results">
+        <p id="search-results-where"> No results found. </p>
+      </div>
     </div>
   );
 }
