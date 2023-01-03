29633dfb3eead1ef65ba0c4ae1d69eacfe40600a
John Diamond <jdiamond@stalag13.net>
2023-01-03 12:51:32 -0600

    Regenerated the telephone-index React app with the TypeScript template.

--- telephone-index/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('Given nothing, when launched, then the header is rendered', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Telephone Index/i);
-  expect(linkElement).toBeInTheDocument();
-});
