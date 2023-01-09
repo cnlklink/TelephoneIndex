96f98dfc87e479aad7c858f19368fe2a05a0f53f
John Diamond <jdiamond@stalag13.net>
2023-01-09 11:03:10 -0600

    Typo in className

--- telephone-index/src/components/QuickIndexItem.tsx
+++ telephone-index/src/components/QuickIndexItem.tsx
@@ -1,9 +1,11 @@
+import './QuickIndex.css'
+
 import React from 'react';
 
 function QuickIndexItem( props: { text: string } ) 
 {
   return (
-    <div className='quick-item-item'>
+    <div className='quick-index-item'>
         { props.text }
     </div>
   );
