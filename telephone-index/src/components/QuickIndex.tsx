938c84f8fad20e63f8eb4d8f95e9fb5b3039dbe2
John Diamond <jdiamond@stalag13.net>
2023-01-09 09:46:10 -0600

    REFACTOR - dynamically generate the quick index

--- telephone-index/src/components/QuickIndex.tsx
+++ telephone-index/src/components/QuickIndex.tsx
@@ -6,10 +6,25 @@ function QuickIndex()
 {
   return (
     <div id="quick-index">
-        <div className="quick-index-item"> A </div>
-        <div className="quick-index-item"> B </div>
+        { 
+            _getItems().map( (item, _) => {
+                return <div className="quick-index-item"> {item} </div>
+            } )
+        }
     </div>
 );
+}
+
+function _getItems(): Array<string>
+{
+    const alphabet: Array<string> = []
+
+    for( let i = 65; i <= 90; i++ )
+    {
+        alphabet.push( String.fromCharCode( i ) )
+    }
+
+    return alphabet
 }
 
 export default QuickIndex;
