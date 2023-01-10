66c9117014216fa828c9cbec70d3bb27eb979bd8
John Diamond <jdiamond@stalag13.net>
2023-01-10 08:23:38 -0600

    Stubbed out an onChange handler for search criteria input

--- telephone-index/src/components/Search/SearchBar.tsx
+++ telephone-index/src/components/Search/SearchBar.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 
 function SearchBar() 
 {
-  return (
-    <div id="search-bar">
-        <form>
-            <div className="search-bar__controls">
-                <div className="search-bar__control">
-                    <label> Search: </label>
-                    <input name="search-criteria" type="text"/>
+    const searchCriteriaChangedHandler = () => {
+        console.log( "Search criteria changed!" )
+    }
+
+    return (
+        <div id="search-bar">
+            <form>
+                <div className="search-bar__controls">
+                    <div className="search-bar__control">
+                        <label> Search: </label>
+                        <input name="search-criteria" type="text" onChange={searchCriteriaChangedHandler}/>
+                    </div>
                 </div>
-            </div>
-        </form>
-    </div>
+            </form>
+        </div>
     )
 }
 
