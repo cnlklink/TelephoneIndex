ea9d25dbd81c33f50b2d7f2bee5f5f640bff402a
John Diamond <jdiamond@stalag13.net>
2023-01-11 08:31:06 -0600

    REFACTOR - created helper functions for initialize App state and publishing search results to that state

--- telephone-index/src/App.tsx
+++ telephone-index/src/App.tsx
@@ -4,33 +4,52 @@ import QuickIndex from './components/QuickIndex/QuickIndex'
 import Search from './components/Search/Search'
 import './App.css';
 
+type SearchResults = {
+  count: number
+}
+
+let _numberOfSearchResults: number
+let _setNumberOfSearchResults: ( numberOfSearchResults: number ) => void 
+
 function App() {
+
   useEffect( () => {
     document.title = 'Telephone Index'
   }, [] );
 
-  const [numberOfSearchResults, setNumberOfSearchResults] = useState<number>( 0 )
-
-  const quickIndexItemSelectedHandler = ( item: string ) => {
-    if( item === 'A' )
-    {
-      setNumberOfSearchResults( 1 )
-    }
-    else
-    {
-      setNumberOfSearchResults( 0 )
-    }
-  }
+  _initializeReactState()
 
   return (
     <div className="Telephone-Index-App">
       <Header/>
 
-      <QuickIndex onItemSelected={quickIndexItemSelectedHandler}/>
+      <QuickIndex onItemSelected= { _quickIndexItemSelectedHandler } />
 
-      <Search numberOfSearchResults={numberOfSearchResults}/>
+      <Search numberOfSearchResults= { _numberOfSearchResults } />
     </div>
   );
+}
+
+function _initializeReactState()
+{
+  [_numberOfSearchResults, _setNumberOfSearchResults] = useState<number>( 0 )
+}
+
+function _quickIndexItemSelectedHandler( item: string )
+{
+  if( item === 'A' )
+  {
+    _publishSearchResults( { count: 1 } )
+  }
+  else
+  {
+    _publishSearchResults( { count: 0 } )
+  }
+}
+
+function _publishSearchResults( searchResults: SearchResults )
+{
+  _setNumberOfSearchResults( searchResults.count )
 }
 
 export default App;
