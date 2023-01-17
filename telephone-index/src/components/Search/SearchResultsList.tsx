926c5495e6e3c1db8d4f324132ccf417c2422572
John Diamond <jdiamond@stalag13.net>
2023-01-17 09:23:00 -0600

    REFACTOR - dynamically build the result list from the searchResults state

--- telephone-index/src/components/Search/SearchResultsList.tsx
+++ telephone-index/src/components/Search/SearchResultsList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { resourceLimits } from 'worker_threads';
 import { SearchResults } from '../../service/TelephoneIndex';
 
 type SearchResultsListProps = {
@@ -7,12 +8,16 @@ type SearchResultsListProps = {
 
 function SearchResultsList( props: SearchResultsListProps ) 
 {
+  const results = props.searchResults.entries
+
   return (
     <div id="search-results">
         <div id="search-results-list">
-          <div className="search-result">
-            Apple, Adam
-          </div>
+          { 
+              results.map( (result, _) => {
+                  return <div className = "search-result">{result.name}</div>
+              } )
+          }
         </div>
         <p id="search-results-where"> 
           { props.searchResults.count === 0 ? "No results found." : props.searchResults.count + " results found." }
